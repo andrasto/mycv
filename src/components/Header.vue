31eabda9161a5ad677dbc89be7dfa84b973c0295
Tóth András <andraas.toth@gmail.com>
2019-04-12 21:15:17 +0200

    Basic header component added

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>András Tóth</h1>
+        <a href="https://www.linkedin.com/in/andrasto/">linkedin.com/in/andrasto</a>
+        <a href="mailto:andraas.toth@gmail.com">andraas.toth@gmail.com</a>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+    div {
+        h1 {
+            margin: 0;
+            padding-top: 2rem;
+        }
+
+        a {
+            display: block;
+        }
+    }
+</style>
+>
+    
+</style>
